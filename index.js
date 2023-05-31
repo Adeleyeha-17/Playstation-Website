9461f146024c3e41dc8ac4457748053b1e95c1ed
unknown <Adeleyeha.17@protonmail.com>
2023-05-31 11:05:26 +0100

    completed nav bar across mobile screenview with javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+ const menuIcon = document.querySelector('.menu-icon')
+ const menuList = document.querySelector('.menu-list')
+
+ menuIcon.addEventListener('click', function(){
+  menuList.classList.toggle('show-list')
+ })
